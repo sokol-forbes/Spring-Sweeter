00c75e4dd7d103292be92616a72ea45ff6ddc64a
sokol-forbes <61580663+sokol-forbes@users.noreply.github.com>
2024-11-26 15:49:04 +0300

    Фильтрация сообщений по тэгу, защита от DOS, логин

--- src/main/java/by/tutorials/sweater/GreatingController.java
+++ src/main/java/by/tutorials/sweater/GreatingController.java
@@ -4,39 +4,52 @@ import by.tutorials.sweater.domain.Message;
 import by.tutorials.sweater.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 public class GreatingController {
     @Autowired
-    private  MessageRepo messageRepo;
+    private MessageRepo messageRepo;
 
     public GreatingController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String,Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+//        model.put("name", name);
         return "greeting";
     }
-    @GetMapping
+
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
-        model.put("message", "Hello Sasha");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("message", messages);
         return "main";
 
     }
-    @PostMapping
+
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages = null;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else {messages = messageRepo.findAll();}
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/by/tutorials/sweater/repo/MessageRepo.java
+++ src/main/java/by/tutorials/sweater/repo/MessageRepo.java
@@ -3,7 +3,9 @@ package by.tutorials.sweater.repo;
 import by.tutorials.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-
+    List<Message> findByTag(String tag);
 }
